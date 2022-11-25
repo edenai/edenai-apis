25e8051e6a0c0db8415c1477587f83b9235c2b74
coscialp <corentin.scialpi@gmail.com>
2022-11-25 11:19:08 +0100

    [refacto] google with new STD and SentimentEnum

--- edenai_apis/features/text/sentiment_analysis/__init__.py
+++ edenai_apis/features/text/sentiment_analysis/__init__.py
@@ -2,4 +2,5 @@ from .sentiment_analysis_args import sentiment_analysis_arguments
 from .sentiment_analysis_dataclass import (
     SentimentAnalysisDataClass,
     SegmentSentimentAnalysisDataClass,
+    SentimentEnum,
 )
