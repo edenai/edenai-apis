073b889858a97fd60d4f122e871d5f856fa9ea19
Adnane Guettaf <adnane.guettaf@edenai.co>
2022-11-28 17:53:39 +0100

    [fix] adjust stt tests args

--- edenai_apis/features/audio/speech_to_text_async/speech_to_text_async_args.py
+++ edenai_apis/features/audio/speech_to_text_async/speech_to_text_async_args.py
@@ -7,4 +7,9 @@ data_path = os.path.join(feature_path, "data")
 
 
 def speech_to_text_arguments() -> Dict:
-    return {"file": open(f"{data_path}/conversation.mp3", "rb"), "language": "en", "speakers" : 2}
+    return {
+        "file": open(f"{data_path}/conversation.mp3", 
+        "rb"), "language": "en", 
+        "speakers" : 2, 
+        "profanity_filter": False
+        }
