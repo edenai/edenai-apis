c900b30787c96d54cb90839f9392b543f8262222
coscialp <corentin.scialpi@gmail.com>
2022-11-29 11:02:11 +0100

    [fix] repair emvista with NaN value

--- edenai_apis/apis/emvista/emvista_api.py
+++ edenai_apis/apis/emvista/emvista_api.py
@@ -152,6 +152,8 @@ class EmvistaApi(ProviderApi, Text):
         return result
 
     def _normalize_sentiment(self, rate: float) -> SentimentEnum:
+        if rate == 'NaN':
+            return SentimentEnum.NEUTRAL
         if rate > 0:
             return SentimentEnum.POSITIVE
         if rate < 0:
@@ -185,10 +187,12 @@ class EmvistaApi(ProviderApi, Text):
         response = requests.post(url, headers=headers, json=files)
         original_response = response.json()
 
+        print(original_response['result']['globalScore'])
+
         standarized_response = SentimentAnalysisDataClass(
             text=text,
             general_sentiment=self._normalize_sentiment(original_response['result']['globalScore']),
-            general_sentiment_rate=abs(original_response['result']['globalScore'])
+            general_sentiment_rate=abs(original_response['result']['globalScore']) if original_response['result']['globalScore'] != 'NaN' else 0
         )
         
         result = ResponseType[SentimentAnalysisDataClass](
