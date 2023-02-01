ae8b2c5976e5d1569f0f13bde883b20cb8dc7db0
coscialp <corentin.scialpi@gmail.com>
2023-02-01 09:20:47 +0100

    [hotfix] choose type in convert_string_to_number amazon_invoice

--- edenai_apis/apis/amazon/amazon_ocr_api.py
+++ edenai_apis/apis/amazon/amazon_ocr_api.py
@@ -350,7 +350,7 @@ class AmazonOcrApi(OcrInterface):
                     item_lines.append(
                         ItemLinesInvoice(
                             description=parsed_items.get("ITEM"),
-                            quantity=convert_string_to_number(parsed_items.get("QUANTITY")),
+                            quantity=convert_string_to_number(parsed_items.get("QUANTITY"), int),
                             amount=convert_string_to_number(
                                 parsed_items.get("PRICE"), float
                             ),
