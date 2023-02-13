971ceebdda52fd5b23bc168d1a4f292fa1220099
Daggx <abdelghanikaoula@gmail.com>
2023-02-13 10:43:57 +0100

    [feat] change args for image generation

--- edenai_apis/features/image/generation/generation_args.py
+++ edenai_apis/features/image/generation/generation_args.py
@@ -3,7 +3,7 @@ from typing import Dict
 def generation_arguments() -> Dict:
     return {
         "text": "A huge red ballon flying outside the city.",
-        "num_images":3,
-        "width" : 1024,
-        "height" : 1024,
+        "num_images":2,
+        "width" : 512,
+        "height" : 512,
         }
