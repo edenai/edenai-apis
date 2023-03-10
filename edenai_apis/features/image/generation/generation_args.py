b01f4c36a31771ce36f520b1252d6ad9cb08961c
Daggx <abdelghanikaoula@gmail.com>
2023-03-10 11:24:19 +0100

    [fix] resolution tests

--- edenai_apis/features/image/generation/generation_args.py
+++ edenai_apis/features/image/generation/generation_args.py
@@ -3,6 +3,6 @@ from typing import Dict
 def generation_arguments() -> Dict:
     return {
         "text": "A huge red ballon flying outside the city.",
-        "resolution" : "1024x1024",
+        "resolution" : "512x512",
         "num_images":1,
         }
