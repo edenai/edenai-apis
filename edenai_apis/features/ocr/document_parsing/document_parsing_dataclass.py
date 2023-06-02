990920e9a7063029831395c55f26c1400badc2f9
coscialp <corentin.scialpi@gmail.com>
2023-06-02 11:06:56 +0200

    [fix] round confidence score in document_parsing

--- edenai_apis/features/ocr/document_parsing/document_parsing_dataclass.py
+++ edenai_apis/features/ocr/document_parsing/document_parsing_dataclass.py
@@ -1,5 +1,5 @@
 from typing import Any, Sequence, Optional
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, validator
 from edenai_apis.utils.bounding_box import BoundingBox
 
 
@@ -8,6 +8,13 @@ class ItemDocumentParsing(BaseModel):
     value: Any
     bounding_box: BoundingBox
     confidence_score: Optional[float] = Field(..., ge=0, le=1)
+
+    @validator("confidence_score")
+    @classmethod
+    def round_confidence_score(cls, v) -> Optional[float]:
+        if v is not None:
+            v = round(v, 2)
+        return v
 
 
 class DocumentParsingDataClass(BaseModel):
