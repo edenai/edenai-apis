197ecc125b3b1dc7e60b50e3215b526f77ba0477
Gaurav Datta <28401566+gdatta01@users.noreply.github.com>
2023-07-10 15:52:32 -0700

    add QA API (#6)

--- edenai_apis/apis/tenstorrent/tenstorrent_text_api.py
+++ edenai_apis/apis/tenstorrent/tenstorrent_text_api.py
@@ -1,10 +1,14 @@
 import requests
+from typing import List, Optional
 
 from edenai_apis.features.text.keyword_extraction.keyword_extraction_dataclass import (
     KeywordExtractionDataClass,
 )
 from edenai_apis.features.text.sentiment_analysis.sentiment_analysis_dataclass import (
     SentimentAnalysisDataClass,
+)
+from edenai_apis.features.text.question_answer.question_answer_dataclass import (
+    QuestionAnswerDataClass,
 )
 from edenai_apis.features.text.text_interface import TextInterface
 from edenai_apis.utils.exception import ProviderException
@@ -66,6 +70,38 @@ class TenstorrentTextApi(TextInterface):
         )
 
         return ResponseType[SentimentAnalysisDataClass](
+            original_response=original_response,
+            standardized_response=standardized_response,
+        )
+
+    def text__question_answer(
+        self,
+        texts: List[str],
+        question: str,
+        temperature: float,
+        examples_context: str,
+        examples: List[List[str]],
+        model: Optional[str],
+    ) -> ResponseType[QuestionAnswerDataClass]:
+        base_url = "https://question-answer--eden-ai.workload.tenstorrent.com"
+        url = f"{base_url}/predictions/question_answer"
+        payload = {
+            "text": texts[0],
+            "question": question,
+        }
+        try:
+            original_response = requests.post(url, json=payload, headers=self.headers)
+            original_response.raise_for_status()
+        except Exception as exc:
+            raise ProviderException(original_response.text)
+
+        original_response = original_response.json()
+
+        # Check for errors
+        self.check_for_errors(original_response)
+
+        standardized_response = QuestionAnswerDataClass(answers=[original_response["answer"]])
+        return ResponseType[QuestionAnswerDataClass](
             original_response=original_response,
             standardized_response=standardized_response,
         )
