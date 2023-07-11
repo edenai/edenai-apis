669523deb27a0dfab97e1451347cb33d276f1619
Daggx <abdelghanikaoula@gmail.com>
2023-07-11 10:52:51 +0200

    [feat] prompt optimization

---
+++ edenai_apis/features/text/prompt_optimization/prompt_optimization_dataclass.py
@@ -0,0 +1,10 @@
+from typing import Sequence
+from pydantic import BaseModel, StrictStr, Field
+
+
+class PromptDataClass(BaseModel):
+    text : StrictStr
+    
+class PromptOptimizationDataClass(BaseModel):
+    items: Sequence[PromptDataClass] = Field(default_factory=list)
+
