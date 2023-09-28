4c7af54cc75bdc321fb3a335bbeebada965b6292
coscialp <corentin.scialpi@gmail.com>
2023-09-28 15:39:55 +0200

    [feat] add assert who handle empty response in sync and async feature

--- edenai_apis/utils/conversion.py
+++ edenai_apis/utils/conversion.py
@@ -295,3 +295,31 @@ def construct_word_list(original_text, corrected_words):
         word_list.append(word_info)
 
     return word_list
+
+
+def iterate_all(iterable, returned="key"):
+    """Returns an iterator that returns all keys or values
+    of a (nested) iterable.
+
+    Arguments:
+        - iterable: <list> or <dictionary>
+        - returned: <string> "key" or "value"
+
+    Returns:
+        - <iterator>
+    """
+    if isinstance(iterable, dict):
+        for key, value in iterable.items():
+            if returned == "key":
+                yield key
+            elif returned == "value":
+                if not (isinstance(value, dict) or isinstance(value, list)):
+                    yield value
+            else:
+                raise ValueError("'returned' keyword only accepts 'key' or 'value'.")
+            for ret in iterate_all(value, returned=returned):
+                yield ret
+    elif isinstance(iterable, list):
+        for el in iterable:
+            for ret in iterate_all(el, returned=returned):
+                yield ret
