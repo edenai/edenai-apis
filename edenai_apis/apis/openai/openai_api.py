43c74be742122a3dcf65dfd298314aaa26e4d1d3
KyrianC <ckyrian@protonmail.com>
2023-10-12 11:10:17 +0200

    [feat] allow multiple api keys for openai

--- edenai_apis/apis/openai/openai_api.py
+++ edenai_apis/apis/openai/openai_api.py
@@ -1,11 +1,13 @@
+import random
 from typing import Dict
-from edenai_apis.loaders.loaders import load_provider
-from edenai_apis.features.provider.provider_interface import ProviderInterface
-from edenai_apis.loaders.data_loader import ProviderDataEnum
+
+from edenai_apis.apis.openai.openai_audio_api import OpenaiAudioApi
 from edenai_apis.apis.openai.openai_image_api import OpenaiImageApi
 from edenai_apis.apis.openai.openai_text_api import OpenaiTextApi
 from edenai_apis.apis.openai.openai_translation_api import OpenaiTranslationApi
-from edenai_apis.apis.openai.openai_audio_api import OpenaiAudioApi
+from edenai_apis.features.provider.provider_interface import ProviderInterface
+from edenai_apis.loaders.data_loader import ProviderDataEnum
+from edenai_apis.loaders.loaders import load_provider
 
 
 class OpenaiApi(
@@ -21,8 +23,11 @@ class OpenaiApi(
         self.api_settings = load_provider(
             ProviderDataEnum.KEY, self.provider_name, api_keys=api_keys
         )
-        self.api_key = self.api_settings["api_key"]
-        self.org_key = self.api_settings["org_key"]
+
+        chosen_api_setting = random.choice(self.api_settings)
+
+        self.api_key = chosen_api_setting["api_key"]
+        self.org_key = chosen_api_setting["org_key"]
         self.url = "https://api.openai.com/v1"
         self.model = "gpt-3.5-turbo-instruct"
         self.headers = {
