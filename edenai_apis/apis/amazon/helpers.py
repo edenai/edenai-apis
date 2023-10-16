4d1c673a6914ec2aba61207b3a582a6b6de3fde6
Daggx <abdelghanikaoula@gmail.com>
2023-10-16 16:58:45 +0200

    [refacto] change amazon video response

--- edenai_apis/apis/amazon/helpers.py
+++ edenai_apis/apis/amazon/helpers.py
@@ -1,8 +1,6 @@
-import json
 import urllib
-from io import BufferedReader
 from time import time
-from typing import Callable, Dict, List, Optional, Tuple, TypeVar, Sequence, Union
+from typing import Callable, Dict, List, Optional, Tuple, TypeVar, Sequence
 from pathlib import Path
 import requests
 from edenai_apis.features.ocr.custom_document_parsing_async.custom_document_parsing_async_dataclass import (
@@ -39,6 +37,27 @@ from edenai_apis.features.ocr.receipt_parser.receipt_parser_dataclass import (
     ItemLines,
     Locale,
 )
+from edenai_apis.features.video.person_tracking_async.person_tracking_async_dataclass import (
+    PersonLandmarks,
+    PersonTracking,
+    VideoPersonPoses,
+    VideoPersonQuality,
+    VideoTrackingBoundingBox,
+    VideoTrackingPerson,
+)
+from edenai_apis.features.video.text_detection_async.text_detection_async_dataclass import (
+    VideoText,
+    VideoTextBoundingBox,
+    VideoTextFrames,
+)
+from edenai_apis.features.video.label_detection_async.label_detection_async_dataclass import (
+    VideoLabel,
+    VideoLabelBoundingBox,
+    VideoLabelTimeStamp,
+)
+from edenai_apis.features.video.explicit_content_detection_async.explicit_content_detection_async_dataclass import (
+    ContentNSFW
+    )
 from trp import Document
 
 from edenai_apis.features.ocr.ocr_tables_async.ocr_tables_async_dataclass import (
@@ -49,21 +68,24 @@ from edenai_apis.features.ocr.ocr_tables_async.ocr_tables_async_dataclass import
     Row,
     Table,
 )
+from edenai_apis.features.video.face_detection_async.face_detection_async_dataclass import (
+    FaceAttributes,
+    FaceDetectionAsyncDataClass,
+    LandmarksVideo,
+    VideoBoundingBox,
+    VideoFace,
+    VideoFacePoses,
+)
 from edenai_apis.loaders.data_loader import ProviderDataEnum
 from edenai_apis.loaders.loaders import load_provider
-from edenai_apis.utils.audio import validate_audio_attribute_against_ssml_tags_use
 from edenai_apis.utils.exception import (
     AsyncJobException,
     AsyncJobExceptionReason,
-    LanguageException,
     ProviderException,
 )
 from edenai_apis.utils.ssml import convert_audio_attr_in_prosody_tag
 
 from edenai_apis.utils.types import (
-    AsyncBaseResponseType,
-    AsyncPendingResponseType,
-    AsyncResponseType,
     ResponseType,
 )
 
@@ -237,24 +259,6 @@ def amazon_video_original_response(
 
     response = handle_amazon_call(function_to_call, **params)
     return response
-
-
-def amazon_video_response_formatter(
-    response: Dict, standardized_response: T, provider_job_id: str
-) -> AsyncBaseResponseType[T]:
-    if response["JobStatus"] == "SUCCEEDED":
-        return AsyncResponseType[T](
-            original_response=response,
-            standardized_response=standardized_response,
-            provider_job_id=provider_job_id,
-        )
-    elif response["JobStatus"] == "IN_PROGRESS":
-        return AsyncPendingResponseType[T](provider_job_id=provider_job_id)
-    elif response["JobStatus"] == "FAILED":
-        error: Optional[str] = response.get("StatusMessage")
-        raise ProviderException(error)
-    raise ProviderException("Amazon did not return a JobStatus")
-
 
 def amazon_custom_document_parsing_formatter(
     pages: List[dict],
@@ -724,3 +728,216 @@ def handle_amazon_call(func: Callable, **kwargs):
             raise AsyncJobException(reason=AsyncJobExceptionReason.DEPRECATED_JOB_ID)
         raise ProviderException(str(exc))
     return response
+
+
+def amazon_video_person_tracking_parser(response):
+    # gather all persons with the same index :
+    persons_index = {index["Person"]["Index"] for index in response["Persons"]}
+    tracked_persons = []
+    for index in persons_index:
+        detected_persons = [
+            item
+            for item in response["Persons"]
+            if item["Person"]["Index"] == index
+        ]
+        tracked_person = []
+        for detected_person in detected_persons:
+            if detected_person["Person"].get("BoundingBox"):
+                offset = float(detected_person["Timestamp"] / 1000.0)
+                bounding_box = detected_person.get("Person").get("BoundingBox")
+                bounding_box = VideoTrackingBoundingBox(
+                    top=bounding_box.get("Top", 0),
+                    left=bounding_box.get("Left", 0),
+                    height=bounding_box.get("Height", 0) ,
+                    width=bounding_box.get("Width", 0),
+                )
+                face = detected_person["Person"].get("Face")
+                # Get landmarks
+                poses = VideoPersonPoses.default()
+                landmarks = PersonLandmarks()
+                quality = VideoPersonQuality.default()
+                if face:
+                    landmarks_dict = {}
+                    for land in face.get("Landmarks", []):
+                        landmarks_dict[land["Type"]] = [land["X"], land["Y"]]
+                    landmarks = PersonLandmarks(
+                        eye_left=landmarks_dict.get("eyeLeft", []),
+                        eye_right=landmarks_dict.get("eyeRight", []),
+                        nose=landmarks_dict.get("nose", []),
+                        mouth_left=landmarks_dict.get("mouthLeft", []),
+                        mouth_right=landmarks_dict.get("mouthRight", []),
+                    )
+                    poses = VideoPersonPoses(
+                        roll=face.get("Pose").get("Roll"),
+                        yaw=face.get("Pose").get("Yaw"),
+                        pitch=face.get("Pose").get("Pitch"),
+                    )
+                    quality = VideoPersonQuality(
+                        brightness=face.get("Quality").get("Brightness"),
+                        sharpness=face.get("Quality").get("Sharpness"),
+                    )
+
+                tracked_person.append(
+                    PersonTracking(
+                        offset=offset,
+                        bounding_box=bounding_box,
+                        landmarks=landmarks,
+                        poses=poses,
+                        quality=quality,
+                    )
+                )
+        if len(tracked_person) > 0 :
+            tracked_persons.append(VideoTrackingPerson(tracked=tracked_person))
+    return tracked_persons
+
+
+def amazon_video_labels_parser(response):
+    labels = []
+    for label in response["Labels"]:
+        # Category
+        parents = []
+        for parent in label["Label"]["Parents"]:
+            if parent["Name"]:
+                parents.append(parent["Name"])
+
+        # bounding boxes
+        boxes = []
+        for instance in label["Label"]["Instances"]:
+            video_box = VideoLabelBoundingBox(
+                top=instance["BoundingBox"].get("Top", 0),
+                left=instance["BoundingBox"].get("Left", 0),
+                width=instance["BoundingBox"].get("Width", 0),
+                height=instance["BoundingBox"].get("Height", 0),
+            )
+            boxes.append(video_box)
+
+        videolabel = VideoLabel(
+            timestamp=[
+                VideoLabelTimeStamp(
+                    start=float(label["Timestamp"]) / 1000.0, end=None
+                )
+            ],
+            confidence=label["Label"].get("Confidence", 0) / 100,
+            name=label["Label"]["Name"],
+            category=parents,
+            bounding_box=boxes,
+        )
+        labels.append(videolabel)
+    return labels
+
+
+def amazon_video_text_parser(response):
+    text_video = []
+    # Get unique values of detected text annotation
+    detected_texts = {
+        text["TextDetection"]["DetectedText"]
+        for text in response["TextDetections"]
+    }
+
+    # For each unique value, get all the frames where it appears
+    for text in detected_texts:
+        annotations = [
+            item
+            for item in response["TextDetections"]
+            if item["TextDetection"]["DetectedText"] == text
+        ]
+        frames = []
+        for annotation in annotations:
+            timestamp = float(annotation["Timestamp"]) / 1000.0
+            confidence = round(
+                annotation["TextDetection"]["Confidence"] / 100, 2
+            )
+            geometry = annotation["TextDetection"]["Geometry"]["BoundingBox"]
+            bounding_box = VideoTextBoundingBox(
+                top=geometry.get("Top", 0),
+                left=geometry.get("Left", 0),
+                width=geometry.get("Width", 0),
+                height=geometry.get("Height", 0),
+            )
+            frame = VideoTextFrames(
+                timestamp=timestamp,
+                confidence=confidence,
+                bounding_box=bounding_box,
+            )
+            frames.append(frame)
+
+        video_text = VideoText(
+            text=text,
+            frames=frames,
+        )
+        text_video.append(video_text)
+
+    return text_video
+
+
+def amazon_video_face_parser(response):
+    faces = []
+    for face in response["Faces"]:
+        # Time stamp
+        offset = float(face["Timestamp"]) / 1000.0  # convert to seconds
+
+        # Bounding box
+        bounding_box = VideoBoundingBox(
+            top=face["Face"]["BoundingBox"].get("Top", 0),
+            left=face["Face"]["BoundingBox"].get("Left", 0),
+            height=face["Face"]["BoundingBox"].get("Height", 0),
+            width=face["Face"]["BoundingBox"].get("Width", 0),
+        )
+
+        # Attributes
+        poses = VideoFacePoses(
+            pitch=face["Face"]["Pose"].get("Pitch", 0) / 100,
+            yawn=face["Face"]["Pose"].get("Yaw", 0) / 100,
+            roll=face["Face"]["Pose"].get("Roll", 0) / 100,
+        )
+        attributes_video = FaceAttributes(
+            pose=poses,
+            brightness=face["Face"]["Quality"].get("Brightness", 0) / 100,
+            sharpness=face["Face"]["Quality"].get("Sharpness", 0) / 100,
+            headwear=None,
+            frontal_gaze=None,
+            eyes_visible=None,
+            glasses=None,
+            mouth_open=None,
+            smiling=None,
+        )
+
+        # Landmarks
+        landmarks_output = {}
+        for land in face["Face"]["Landmarks"]:
+            if land.get("Type") and land.get("X") and land.get("Y"):
+                landmarks_output[land["Type"]] = [land["X"], land["Y"]]
+
+        landmarks_video = LandmarksVideo(
+            eye_left=landmarks_output.get("eyeLeft", []),
+            eye_right=landmarks_output.get("eyeRight", []),
+            mouth_left=landmarks_output.get("mouthLeft", []),
+            mouth_right=landmarks_output.get("mouthRight", []),
+            nose=landmarks_output.get("nose", []),
+        )
+        faces.append(
+            VideoFace(
+                offset=offset,
+                attributes=attributes_video,
+                landmarks=landmarks_video,
+                bounding_box=bounding_box,
+            )
+        )
+    return faces
+
+
+def amazon_video_explicit_parser(response):
+    moderated_content = []
+    for label in response.get("ModerationLabels", []):
+        confidence = label.get("ModerationLabel", {}).get("Confidence")
+        timestamp = float(label.get("Timestamp")) / 1000.0  # convert to seconds
+        if label.get("ParentName"):
+            category = label.get("ParentName", label.get("Name"))
+            moderated_content.append(
+                ContentNSFW(
+                    timestamp=timestamp,
+                    confidence=confidence,
+                    category=category,
+                )
+            )
+    return moderated_content
