235be5a518c5a96b93375b55af3f5ef9751b8517
Daggx <abdelghanikaoula@gmail.com>
2023-11-24 11:12:07 +0100

    [feat] change anthropic to amazon bedrock

--- edenai_apis/apis/anthropic/anthropic_api.py
+++ edenai_apis/apis/anthropic/anthropic_api.py
@@ -1,17 +1,12 @@
 from typing import Dict
-
-from clarifai_grpc.channel.clarifai_channel import ClarifaiChannel
-from clarifai_grpc.grpc.api import resources_pb2, service_pb2, service_pb2_grpc
-from clarifai_grpc.grpc.api.status import status_code_pb2
-from google.protobuf.json_format import MessageToDict
-
 from edenai_apis.features import ProviderInterface, TextInterface
 from edenai_apis.features.text import GenerationDataClass
 from edenai_apis.loaders.data_loader import ProviderDataEnum
 from edenai_apis.loaders.loaders import load_provider
-from edenai_apis.utils.exception import ProviderException
 from edenai_apis.utils.types import ResponseType
-
+from edenai_apis.apis.amazon.helpers import handle_amazon_call
+import boto3
+import json
 
 class AnthropicApi(
     ProviderInterface,
@@ -24,54 +19,43 @@ class AnthropicApi(
         self.api_settings = load_provider(
             ProviderDataEnum.KEY, self.provider_name, api_keys=api_keys
         )
-        self.user_id = "anthropic"
-        self.app_id = "completion"
-        self.key = self.api_settings["api_key"]
-    
+        self.bedrock = boto3.client(
+            "bedrock-runtime",
+            region_name=self.api_settings["region_name"],
+            aws_access_key_id=self.api_settings["aws_access_key_id"],
+            aws_secret_access_key=self.api_settings["aws_secret_access_key"],
+        )
+
     def text__generation(
         self, 
         text: str,
         temperature: float, 
         max_tokens: int,
         model: str,) -> ResponseType[GenerationDataClass]:
-        
-        channel = ClarifaiChannel.get_grpc_channel()
-        stub = service_pb2_grpc.V2Stub(channel)
-        metadata = (("authorization", self.key),)
-        userDataObject = resources_pb2.UserAppIDSet(user_id=self.user_id, app_id=self.app_id)
-        post_model_outputs_response = stub.PostModelOutputs(
-            service_pb2.PostModelOutputsRequest(
-                user_app_id=userDataObject, 
-                model_id=model,
-                inputs=[
-                    resources_pb2.Input(
-                        data=resources_pb2.Data(
-                             text=resources_pb2.Text(
-                                raw=text
-                            )
-                        )
-                    )
-                ]
-            ),
-            metadata=metadata
-        )
-        if post_model_outputs_response.status.code != status_code_pb2.SUCCESS:
-            raise ProviderException(
-                post_model_outputs_response.status.description,
-                code= post_model_outputs_response.status.code
-                )
-        response = MessageToDict(
-            post_model_outputs_response, preserving_proto_field_name=True
-        )
-        output = response.get("outputs", [])
-        if len(output) == 0:
-            raise ProviderException(
-                "Anthropic returned an empty response!",
-                code= post_model_outputs_response.status.code
-                )
-        original_response = output[0].get("data", {}) or {}
-        
+        # Headers for the HTTP request
+        accept_header = 'application/json'
+        content_type_header = 'application/json'
+
+        # Body of the HTTP request, containing text, maxTokens, and temperature
+        request_body = json.dumps({
+            "prompt": f"\n\nHuman:{text}\n\nAssistant:",
+            "temperature": temperature,
+            "max_tokens_to_sample" : max_tokens
+        })
+
+        # Parameters for the HTTP request
+        request_params = {
+            "body": request_body,
+            "modelId": f"{self.provider_name}.{model}",
+            "accept": accept_header,
+            "contentType": content_type_header
+        }
+        response = handle_amazon_call(self.bedrock.invoke_model, **request_params)
+        response_body = json.loads(response.get('body').read())
+        generated_text = response_body['completion']
+        standardized_response = GenerationDataClass(generated_text=generated_text)
+
         return ResponseType[GenerationDataClass](
-            original_response=original_response,
-            standardized_response=GenerationDataClass(generated_text=original_response.get('text', {}).get('raw', '')),
+            original_response=response_body,
+            standardized_response=standardized_response,
         )
