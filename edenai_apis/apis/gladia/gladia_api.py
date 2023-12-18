0740dd9e29e1e2d81655e0769422a2feee67f0b6
Florine Kieraga <florine.kieraga@epita.fr>
2023-12-18 15:22:56 +0100

    [refacto] change gladia to async endpoint

--- edenai_apis/apis/gladia/gladia_api.py
+++ edenai_apis/apis/gladia/gladia_api.py
@@ -1,6 +1,6 @@
-
 import json
 import uuid
+from json import JSONDecodeError
 from pathlib import Path
 from time import time
 from typing import Dict
@@ -9,11 +9,10 @@ import requests
 from apis.amazon.helpers import check_webhook_result
 
 from edenai_apis.features import ProviderInterface, AudioInterface
-from edenai_apis.features.audio import (
-    SpeechDiarizationEntry,
-    SpeechDiarization
+from edenai_apis.features.audio import SpeechDiarizationEntry, SpeechDiarization
+from edenai_apis.features.audio.speech_to_text_async.speech_to_text_async_dataclass import (
+    SpeechToTextAsyncDataClass,
 )
-from edenai_apis.features.audio.speech_to_text_async.speech_to_text_async_dataclass import SpeechToTextAsyncDataClass
 from edenai_apis.loaders.data_loader import ProviderDataEnum
 from edenai_apis.loaders.loaders import load_provider
 from edenai_apis.utils.exception import ProviderException
@@ -31,33 +30,28 @@ class GladiaApi(ProviderInterface, AudioInterface):
     provider_name = "gladia"
 
     def __init__(self, api_keys: Dict = {}) -> None:
-        self.api_settings = load_provider(ProviderDataEnum.KEY, self.provider_name, api_keys = api_keys)
+        self.api_settings = load_provider(
+            ProviderDataEnum.KEY, self.provider_name, api_keys=api_keys
+        )
         self.api_key = self.api_settings["gladia_key"]
-        self.url = "https://api.gladia.io/audio/text/audio-transcription/"
+        self.url = "https://api.gladia.io/v2/transcription/"
         self.webhook_settings = load_provider(ProviderDataEnum.KEY, "webhooksite")
         self.webhook_token = self.webhook_settings["webhook_token"]
 
-
     def audio__speech_to_text_async__launch_job(
-        self, 
+        self,
         file: str,
-        language: str, 
-        speakers: int, 
-        profanity_filter: bool, 
+        language: str,
+        speakers: int,
+        profanity_filter: bool,
         vocabulary: list,
         audio_attributes: tuple,
-        model : str = None,
+        model: str = None,
         file_url: str = "",
         provider_params: dict = dict(),
-        ) -> AsyncLaunchJobResponseType:
-        data_job_id = {}
+    ) -> AsyncLaunchJobResponseType:
+        headers = {"x-gladia-key": self.api_key}
         export_format, channels, frame_rate = audio_attributes
-
-        headers = {
-            "x-gladia-key": self.api_key,
-        }
-
-
         file_name = str(int(time())) + "_" + str(file.split("/")[-1])
 
         content_url = file_url
@@ -65,125 +59,73 @@ class GladiaApi(ProviderInterface, AudioInterface):
             content_url = upload_file_to_s3(
                 file, Path(file_name).stem + "." + export_format
             )
-
-        files = {
-            'audio_url': content_url,
-        }
         data = {
-            'language_behaviour': 'automatic multiple languages',
-            'toggle_diarization': 'true',
+            "audio_url": content_url,
+            "detect_language": True,
+            "enable_code_switching": True,
+            "custom_vocabulary": vocabulary,
+            "diarization": True,
+            "diarization_config": {"number_of_speakers": speakers},
         }
-
+        # # Change languages in info
         if language:
-            data.update({
-                'language': language_matches[language],
-                'language_behaviour': 'manual',
-            })
+            data.update({"detect_language": False, "language": language})
         data.update(provider_params)
+        response = requests.post(self.url, headers=headers, json=data)
+        if response.status_code != 201:
+            raise ProviderException(message=response.text, code=response.status_code)
+        try:
+            original_response = response.json()
+        except JSONDecodeError:
+            raise ProviderException(message="Internal Server Error", code=500)
+        return AsyncLaunchJobResponseType(
+            provider_job_id=original_response.get("id", "")
+        )
 
-
-        response = requests.post('https://api.gladia.io/audio/text/audio-transcription/', headers=headers, files=files, data=data)
-
-        original_response = response.json()
-        if response.status_code != 200:
-            # error example
-            """
-            {
-            "statusCode": 400,
-            "timestamp": "2023-07-05T03:02:05.402Z",
-            "path": "/audio/text/audio-transcription/?model=large-v2",
-            "message": "You need to specify at least one of these fields: video, video_url",
-            "request_id": " "
-            }
-            """
-            raise ProviderException(
-                f"{original_response.get('timestamp')} - {original_response.get('statusCode')}: "\
-                f"{original_response.get('message')} - request_id: {original_response.get('request_id')}",
-                code = response.status_code
-            )
-        
-        job_id = "gladia_stt" + str(uuid.uuid4())
-        data_job_id[job_id] = original_response
-        requests.post(
-            url = f'https://webhook.site/{self.webhook_token}',
-            data = json.dumps(data_job_id),
-            
-            headers = {'content-type':'application/json'})
-        return AsyncLaunchJobResponseType(provider_job_id=job_id)
-    
     def audio__speech_to_text_async__get_job_result(
-        self,
-        provider_job_id: str
-        ) -> AsyncBaseResponseType[SpeechToTextAsyncDataClass]:
+        self, provider_job_id: str
+    ) -> AsyncBaseResponseType[SpeechToTextAsyncDataClass]:
         if not provider_job_id:
             raise ProviderException("Job id None or empty!")
-        
-        # Get results from webhooks : 
-        # List all webhook results
-        # Getting results from webhook.site
-
-        wehbook_result, response_status = check_webhook_result(provider_job_id, self.webhook_settings)
-
-        if response_status != 200:
-            raise ProviderException(wehbook_result, code = response_status)
-        
-        result_object = next(filter(lambda response: provider_job_id in response["content"], wehbook_result), None) \
-            if wehbook_result else None
-
-        if not result_object or not result_object.get("content"):
-            raise ProviderException("Provider returned an empty response")
-
+        headers = {"x-gladia-key": self.api_key, "accept": "application/json"}
+        response = requests.get(self.url + provider_job_id, headers=headers)
+        print(response.text)
+        if response.status_code != 200:
+            raise ProviderException(message=response.text, code=response.status_code)
         try:
-            original_response = json.loads(result_object["content"]).get(provider_job_id, None)
-        except json.JSONDecodeError:
-            raise ProviderException("An error occurred while parsing the response.")
-
-        if original_response is None:
+            original_response = response.json()
+        except JSONDecodeError:
+            raise ProviderException(message="Internal Server Error", code=500)
+        if original_response.get("status") == "processing":
             return AsyncPendingResponseType[SpeechToTextAsyncDataClass](
                 provider_job_id=provider_job_id
             )
-        diarization_entries = []
-        speakers = {}
-        index_speaker  = 0
-        text = ""
-        if "prediction" in original_response:
-            for utterance in original_response["prediction"]:
-                text_segment = utterance.get("transcription", "")
-                time_begin = utterance.get("time_begin", 0)
-                time_end = utterance.get("time_end", 0)
-                speaker_id = utterance.get("speaker", 0)
-                confidence = -1
-                text += f"{text_segment} "
-                if speaker_id not in speakers:
-                    index_speaker += 1
-                    speaker_tag = index_speaker
-                    speakers[speaker_id] = index_speaker
-                elif speaker_id in speakers:
-                    speaker_tag = speakers[speaker_id]
-
-                # calculate the average of the confidence of each word
-                for word in utterance.get("words", []):
-                    confidence += word.get("confidence", 0)
-                    
-                confidence = 0 if len(utterance.get("words", [])) == 0 else confidence / len(utterance.get("words", []))
-
-                diarization_entries.append(
+        result = original_response.get("result", {})
+        transcription = result.get("transcription", {})
+        text = transcription.get("full_transcript", " ")
+        entries = []
+        total_speakers = 0
+        for utterance in transcription.get("utterances", []):
+            for word in utterance.get("words", []):
+                entries.append(
                     SpeechDiarizationEntry(
-                        speaker=speaker_tag,
-                        segment=text_segment,
-                        start_time=str(time_begin),
-                        end_time=str(time_end),
-                        confidence=confidence
+                        segment=word.get("word", " "),
+                        start_time=str(word.get("start", 0)),
+                        end_time=str(word.get("end", 0)),
+                        speaker=utterance.get("speaker", 0),
+                        confidence=word.get("confidence", 0),
                     )
                 )
-
-        diarization = SpeechDiarization(total_speakers=len(speakers), entries=diarization_entries)
-        if len(speakers) == 0:
-            diarization.error_message = "Speaker diarization not available for the data specified"
-            
-        standardized_response = SpeechToTextAsyncDataClass(text = text, diarization=diarization)
+            total_speakers = max(utterance.get("speaker", 0), total_speakers)
+        diarization = SpeechDiarization(total_speakers=total_speakers, entries=entries)
+        if total_speakers == 0:
+            diarization.error_message = (
+                "Speaker diarization not available for the data specified"
+            )
         return AsyncResponseType[SpeechToTextAsyncDataClass](
             original_response=original_response,
-            standardized_response=standardized_response,
+            standardized_response=SpeechToTextAsyncDataClass(
+                text=text, diarization=diarization
+            ),
             provider_job_id=provider_job_id,
         )
