0b6b1f922640d44a8d3396090eb6420ae10a843a
coscialp <corentin.scialpi@gmail.com>
2023-12-19 09:55:07 +0100

    [Fix] Add field validator to AutomaticTranslationDataClass
    A field validator has been added to ensure content is present in 'text'. This method checks the 'text' field before processing and replaces empty strings with a default value to avoid errors.

--- edenai_apis/features/translation/automatic_translation/automatic_translation_dataclass.py
+++ edenai_apis/features/translation/automatic_translation/automatic_translation_dataclass.py
@@ -1,11 +1,18 @@
-from typing import Dict
+from typing import Dict, Any
 
-from pydantic import BaseModel, StrictStr
+from pydantic import BaseModel, StrictStr, field_validator
 
 
 class AutomaticTranslationDataClass(BaseModel):
     text: StrictStr
 
+    @classmethod
+    @field_validator("text", mode="before")
+    def check_text(cls, value: StrictStr) -> StrictStr:
+        if not value:
+            value = ""
+        return value
+
     @staticmethod
-    def direct_response(api_response: Dict):
+    def direct_response(api_response: Dict[Any, Any]) -> StrictStr:
         return api_response["text"]
