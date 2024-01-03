c28c14f387e5391dd7bb64b63996cfa04c801786
Daggx <abdelghanikaoula@gmail.com>
2024-01-03 12:31:43 +0100

    [feat] amazon titan in text generation

--- edenai_apis/apis/amazon/config.py
+++ edenai_apis/apis/amazon/config.py
@@ -59,6 +59,12 @@ def clients(api_settings: Dict) -> Dict:
             aws_access_key_id=api_settings["aws_access_key_id"],
             aws_secret_access_key=api_settings["aws_secret_access_key"],
         ),
+        "bedrock" : boto3.client(
+            "bedrock-runtime",
+            region_name="us-east-1",
+            aws_access_key_id=api_settings["aws_access_key_id"],
+            aws_secret_access_key=api_settings["aws_secret_access_key"],
+        )
     }
 
 
