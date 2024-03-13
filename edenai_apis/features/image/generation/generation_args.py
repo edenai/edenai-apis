df7a902eba2cc64ac5b0fd8254d3441bbcac2f05
Daggx <abdelghanikaoula@gmail.com>
2024-03-13 17:25:35 +0100

    change default models for some providers

--- edenai_apis/features/image/generation/generation_args.py
+++ edenai_apis/features/image/generation/generation_args.py
@@ -4,6 +4,6 @@ from typing import Dict
 def generation_arguments(provider_name: str) -> Dict:
     return {
         "text": "A huge red ballon flying outside the city.",
-        "resolution": "512x512",
+        "resolution": "1024x1024",
         "num_images": 1,
     }
