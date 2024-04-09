f325a1b11eb701ebe37fc3b0992f9f24f3072d9d
KyrianC <67210837+KyrianC@users.noreply.github.com>
2024-04-09 09:28:09 +0200

    fix fallback page num in amazon_ocr_api.py

--- edenai_apis/apis/amazon/amazon_ocr_api.py
+++ edenai_apis/apis/amazon/amazon_ocr_api.py
@@ -286,7 +286,7 @@ class AmazonOcrApi(OcrInterface):
         self.storage_clients["textract"].Bucket(self.api_settings["bucket"]).put_object(
             Key=file, Body=file_content
         )
-        pages_num = str(query.get("pages", 1)).split(",") or None
+        pages_num = str(query.get("pages", 1) or 1).split(",") or None
         formatted_queries = [
             {"Text": query.get("query"), "Pages": pages_num}
             for query in queries
