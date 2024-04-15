0f969c2df79dfbec93964c172d1e73e51921d19b
Jasper Hoving <jasper.hoving@gmail.com>
2024-04-15 17:34:44 +0200

    Fix value not exists bug?

--- edenai_apis/features/image/explicit_content/explicit_content_dataclass.py
+++ edenai_apis/features/image/explicit_content/explicit_content_dataclass.py
@@ -47,7 +47,7 @@ class ExplicitItem(BaseModel):
 
     @field_serializer("subcategory", mode="plain", when_used="always")
     def serialize_subcategory(self, value: SubCategoryType, _: FieldSerializationInfo):
-        return value.value
+        return getattr(value, 'value', None)
 
 
 class ExplicitContentDataClass(BaseModel):
