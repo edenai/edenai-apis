1c9418c30e3342049c4b3cad51e9efd5251d9159
KyrianC <ckyrian@protonmail.com>
2024-05-30 16:13:46 +0200

    [fix] rename arg and add to init file

--- edenai_apis/features/image/ai_detection/ai_detection_args.py
+++ edenai_apis/features/image/ai_detection/ai_detection_args.py
@@ -1,6 +1,6 @@
 HUMAN_IMAGE_EXAMPLE = ('https://www.iptc.org/std-dev/photometadata/examples/google-licensable/images/IPTC-GoogleImgSrcPmd_testimg01.jpg')
 
-def ai_image_detection_arguments(provider_name: str) -> dict:
+def ai_detection_arguments(provider_name: str) -> dict:
     return {
-        "url": HUMAN_IMAGE_EXAMPLE
+        "file_url": HUMAN_IMAGE_EXAMPLE
     }
