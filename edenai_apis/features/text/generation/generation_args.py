1075d2c490ae5de2d30e2f99e179cdadadb8cf74
Daggx <abdelghanikaoula@gmail.com>
2024-07-25 16:41:37 +0200

    [Feat] Llama new models

--- edenai_apis/features/text/generation/generation_args.py
+++ edenai_apis/features/text/generation/generation_args.py
@@ -1,7 +1,7 @@
 def generation_arguments(provider_name: str):
     return {
         "text": "The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly.\n\nHuman: Hello, who are you?",
-        "max_tokens": 25,
+        "max_tokens": 500,
         "temperature": 0.8,
         "settings": {
             "amazon": "titan-tg1-large",
@@ -11,6 +11,6 @@ def generation_arguments(provider_name: str):
             "anthropic": "claude-v2",
             "mistral": "large-latest",
             "ai21labs": "j2-ultra",
-            "meta": "llama2-70b-chat-v1",
+            "meta": "llama3-1-70b-instruct-v1:0",
         },
     }
