642b3cf22d7dbb70a76eacc749b3f2a7410fef1b
Daggx <abdelghanikaoula@gmail.com>
2025-01-31 16:27:05 +0100

    Chat/Multimodal in llm engine

--- edenai_apis/llm_engine/llm_engine.py
+++ edenai_apis/llm_engine/llm_engine.py
@@ -1,17 +1,24 @@
 import logging
 import uuid
-from typing import Any, List, Literal, Optional, Type, Union, Dict
+from typing import Any, List, Literal, Optional, Union, Dict
 
 from llm_engine.types.response_types import ResponseModel
 from llm_engine.clients import LLM_COMPLETION_CLIENTS
 from llm_engine.clients.completion import CompletionClient
-from llm_engine.utils import format_chat_messages, convert_tools_to_openai
+from edenai_apis.llm_engine.mapping import Mappings
 from edenai_apis.utils.types import ResponseType
 from edenai_apis.features.text.chat import ChatDataClass, ChatMessageDataClass
 from edenai_apis.features.text.chat.chat_dataclass import (
     StreamChat,
     ChatStreamResponse,
     ToolCall,
+)
+
+from edenai_apis.features.multimodal.chat import (
+    ChatDataClass as ChatMultimodalDataClass,
+    StreamChat as StreamMultimodalChat,
+    ChatMessageDataClass as ChatMultimodalMessageDataClass,
+    ChatStreamResponse as ChatMultimodalStreamResponse,
 )
 
 logger = logging.getLogger(__name__)
@@ -57,7 +64,7 @@ class LLMEngine:
         tool_results: Optional[List[dict]] = None,
         **kwargs,
     ) -> ResponseType[Union[ChatDataClass, StreamChat]]:
-        messages = format_chat_messages(
+        messages = Mappings.format_chat_messages(
             text, chatbot_global_action, previous_history, tool_results
         )
         call_params = {
@@ -68,12 +75,13 @@ class LLMEngine:
             "stream": stream,
             "drop_params": True,
         }
-
         for config_key, config_value in self.provider_config.items():
             call_params[config_key] = config_value
 
         if available_tools and not tool_results:
-            call_params["tools"] = convert_tools_to_openai(available_tools)
+            call_params["tools"] = Mappings.convert_tools_to_openai(
+                tools=available_tools
+            )
             call_params["tool_choice"] = tool_choice
 
         response = self.completion_client.completion(**call_params, **kwargs)
@@ -106,7 +114,7 @@ class LLMEngine:
             )
 
             return ResponseType[ChatDataClass](
-                original_response=response,
+                original_response=response.to_dict(),
                 standardized_response=standardized_response,
                 usage=response.usage,
             )
@@ -116,7 +124,7 @@ class LLMEngine:
                     text=chunk.to_dict()["choices"][0]["delta"].get("content", ""),
                     blocked=not chunk.to_dict()["choices"][0].get("finish_reason")
                     in (None, "stop"),
-                    provider="openai",
+                    provider=self.provider_name,
                 )
                 for chunk in response
                 if chunk
@@ -125,3 +133,80 @@ class LLMEngine:
             return ResponseType[StreamChat](
                 original_response=None, standardized_response=StreamChat(stream=stream)
             )
+
+    def multimodal_chat(
+        self,
+        messages: List[ChatMessageDataClass],
+        chatbot_global_action: Optional[str],
+        temperature: float = 0,
+        max_tokens: int = 25,
+        model: Optional[str] = None,
+        stop_sequences: Optional[List[str]] = None,
+        top_k: Optional[int] = None,
+        top_p: Optional[int] = None,
+        stream: bool = False,
+        provider_params: Optional[dict] = None,
+        response_format=None,
+        tool_choice: Literal["auto", "required", "none"] = "auto",
+        available_tools: Optional[dict] = None,
+        tool_results: Optional[List[dict]] = None,
+        **kwargs,
+    ) -> ResponseType[Union[ChatMultimodalDataClass, StreamMultimodalChat]]:
+
+        transformed_messages = Mappings.format_multimodal_messages(
+            messages=messages, system_prompt=chatbot_global_action
+        )
+        args = {
+            "messages": transformed_messages,
+            "temperature": temperature,
+            "max_tokens": max_tokens,
+            "model": model,
+            "stop_sequences": stop_sequences,
+            "top_p": top_p,
+            "stream": stream,
+        }
+        for config_key, config_value in self.provider_config.items():
+            args[config_key] = config_value
+
+        if available_tools and len(available_tools) > 0 and not tool_results:
+            args["tools"] = Mappings.convert_tools_llmengine(available_tools)
+            args["tool_choice"] = tool_choice
+
+        args["response_format"] = response_format
+        args["drop_invalid_params"] = True
+        response = self.completion_client.completion(**args, **kwargs)
+        response = ResponseModel.model_validate(response)
+        if stream is False:
+            message = response.choices[0].message
+            generated_text = message.content
+
+            standardized_response = (
+                ChatMultimodalDataClass.generate_standardized_response(
+                    generated_text=generated_text, messages=messages
+                )
+            )
+            return ResponseType[ChatMultimodalDataClass](
+                original_response=response.to_dict(),
+                standardized_response=standardized_response,
+                usage=response.usage,
+            )
+
+        else:
+            stream = (
+                ChatMultimodalStreamResponse(
+                    text=chunk["choices"][0]["delta"].get("content", ""),
+                    blocked=not chunk["choices"][0].get("finish_reason")
+                    in (None, "stop"),
+                    provider=self.provider_name,
+                )
+                for chunk in response
+                if chunk
+            )
+
+            return ResponseType[StreamMultimodalChat](
+                original_response=None,
+                standardized_response=StreamMultimodalChat(stream=stream),
+            )
+
+    def summarize(self, text):
+        pass
