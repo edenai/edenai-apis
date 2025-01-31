85fe778ed2c4056357ac831f1709e0c73e9b6b18
Juan David Cruz <juan.cruzgomez@edenai.co>
2025-01-31 11:58:03 +0100

    togetherai old way

--- edenai_apis/apis/__init__.py
+++ edenai_apis/apis/__init__.py
@@ -65,5 +65,6 @@ from .winstonai import WinstonaiApi
 from .writesonic import WritesonicApi
 from .xai import XAiApi
 from .deepseek import DeepseekApi
+from .together_ai import TogetherAiAPI
 
 # THIS NEEDS TO BE DONE AUTOMATICALLY
