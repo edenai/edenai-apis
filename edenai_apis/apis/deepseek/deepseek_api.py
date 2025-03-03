9f20a081c3708b4be6b6d8397b4fd087cd3341b7
Juan David Cruz <juan.cruzgomez@edenai.co>
2025-03-03 17:36:01 +0100

    edenai_apis.

--- edenai_apis/apis/deepseek/deepseek_api.py
+++ edenai_apis/apis/deepseek/deepseek_api.py
@@ -11,7 +11,7 @@ from edenai_apis.utils.types import ResponseType
 from edenai_apis.loaders.loaders import load_provider
 from edenai_apis.loaders.data_loader import ProviderDataEnum
 from edenai_apis.llmengine import LLMEngine
-from features.llm.llm_interface import LlmInterface
+from edenai_apis.features.llm.llm_interface import LlmInterface
 from edenai_apis.llmengine.llm_engine import StdLLMEngine
 from edenai_apis.features.llm.chat.chat_dataclass import ChatCompletionResponse
 
