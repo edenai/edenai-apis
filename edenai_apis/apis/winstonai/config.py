f6d7f698c5ae6504cf8e515e7e28c0e7e43f5e84
Philrobots <philippe.vincent.3@ulaval.ca>
2025-06-18 15:56:29 -0400

    feat(): change winstonai plagiarism url

--- edenai_apis/apis/winstonai/config.py
+++ edenai_apis/apis/winstonai/config.py
@@ -1,2 +1,2 @@
 WINSTON_AI_API_URL = "https://api.gowinston.ai/functions/v1"
-WINSTON_AI_PLAGIA_DETECTION_URL = "https://api.gowinston.ai/plagiarism/v2"
+WINSTON_AI_PLAGIA_DETECTION_URL = "https://api.gowinston.ai/v2/plagiarism"
